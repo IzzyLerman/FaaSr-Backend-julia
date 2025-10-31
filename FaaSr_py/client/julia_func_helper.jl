2241dfce723ee34d51d3834083526a9c7b3c68f1
Izzy Lerman <izzylerman14@gmail.com>
2025-10-30 17:10:01 -0700

    other fix

--- FaaSr_py/client/julia_func_helper.jl
+++ FaaSr_py/client/julia_func_helper.jl
@@ -11,7 +11,7 @@ function faasr_import_function_walk(func_name, directory=".")
     for (path, dirs, files) in Base.Filesystem.walkdir(directory)
         for file in files
             if Base.endswith(file, ".jl") && !(file in ignore_files)
-                include("$path/$file")
+                Main.include("$path/$file")
                 try
                     user_func = getfield(Main, fn)
                     return user_func
