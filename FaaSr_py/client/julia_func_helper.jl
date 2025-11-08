0ef33a11d48e09be7ec2488922eb267c56aed809
Izzy Lerman <izzylerman14@gmail.com>
2025-11-08 14:20:13 -0800

    make sure other julia files don't crash function loading

--- FaaSr_py/client/julia_func_helper.jl
+++ FaaSr_py/client/julia_func_helper.jl
@@ -2,18 +2,21 @@ import Base
 
 function faasr_import_function_walk(func_name, directory=".")
     ignore_files = [
+        "julia_client_stubs.jl",
         "julia_func_helper.jl",
         "julia_user_func_entry.jl"
     ]
 
     fn = Symbol(func_name)
 
+    # TODO ignore files without user function
+
     for (path, dirs, files) in Base.Filesystem.walkdir(directory)
         for file in files
             if Base.endswith(file, ".jl") && !(file in ignore_files)
-                include("$path/$file")
-                println("included $path/$file")
                 try
+                    include("$path/$file")
+                    println("included $path/$file")
                     println("function $func_name: $(isdefined(Main, fn))")
                     if isdefined(Main, fn)
                         return fn
